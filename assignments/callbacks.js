9dd6528743f726e9ad81ebb45e5f55e7981eba4e
Emily Lagattolla <elagattolla@gmail.com>
2019-06-11 15:02:40 -0500

    completed callback functions

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -64,7 +64,16 @@ multiplyNums(8, 7, function(product) {
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  if (list.includes(item)) {
+    return cb(true);
+  } else {
+    return cb(false);
+  }
 }
+
+contains('yo-yo', items, function(bool) {
+  console.log(bool)
+});
 
 /* STRETCH PROBLEM */
 

165d421cac03ded538b43854a2c242daf0648121
Emily Lagattolla <elagattolla@gmail.com>
2019-06-11 14:44:57 -0500

    added second cb function

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -36,7 +36,12 @@ getLength(items, function(length) {
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length - 1])
 }
+
+last(items, function(lastItem) {
+  console.log(lastItem)
+});
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.

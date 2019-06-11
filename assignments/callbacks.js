b9fa6f4a6db7d47db4a24305b8c3b0290d6d32c6
Emily Lagattolla <elagattolla@gmail.com>
2019-06-11 14:49:57 -0500

    added third callback function

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -27,7 +27,7 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
-  return cb(arr.length)
+  return cb(arr.length);
 }
 
 getLength(items, function(length) {
@@ -36,7 +36,7 @@ getLength(items, function(length) {
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
-  return cb(arr[arr.length - 1])
+  return cb(arr[arr.length - 1]);
 }
 
 last(items, function(lastItem) {
@@ -45,7 +45,12 @@ last(items, function(lastItem) {
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y);
 }
+
+sumNums(8, 7, function(total) {
+  console.log(total)
+});
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
